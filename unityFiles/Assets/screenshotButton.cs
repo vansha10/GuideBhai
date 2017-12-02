bbbee0a26d24834a09a1611a1e738d6443183c0c
Vansh Arora <vansha10@gmail.com>
2017-12-02 23:59:41 +0530

    Added photo capturing capabalities within the AR environment

---
+++ unityFiles/Assets/screenshotButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class screenshotButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void Screenshot() {
+		Application.CaptureScreenshot("Capture.png");
+	}
+}
